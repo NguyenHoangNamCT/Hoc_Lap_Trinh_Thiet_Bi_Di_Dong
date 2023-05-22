73d3712f7f270a7b2b33d07262a8f22649a0e437
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-05-22 19:34:42 +0700

    Ứng dụng random số ngầu nhiên trong khoảng người dùng nhập vào

---
+++ 20_TaoSoNgauNhien/UngDungTaoSoNgauNhien/app/src/main/java/com/example/apptaosongaunhien/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.apptaosongaunhien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtKQ;
+    EditText edtMin, edtMax;
+    Button btnRandom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        anhXa();
+
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int min = Integer.parseInt(edtMin.getText().toString());
+                int max = Integer.parseInt(edtMax.getText().toString());
+                if(min > max){
+                    int temp = min;
+                    min = max;
+                    max = temp;
+                }
+                Random r = new Random();
+                txtKQ.setText((r.nextInt(max - min + 1) + min) + "");
+            }
+        });
+    }
+
+    public void anhXa(){
+        txtKQ = findViewById(R.id.textView);
+        edtMin = findViewById(R.id.editTextText);
+        edtMax = findViewById(R.id.editTextText2);
+        btnRandom = findViewById(R.id.button);
+    }
+}
