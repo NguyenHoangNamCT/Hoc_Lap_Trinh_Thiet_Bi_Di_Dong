74d1569681cf1629dd3636a78251c120e9415194
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-05-24 20:22:01 +0700

    SeekBar. Thêm hình cho seekbar, khi bấm vào thì in ra start kéo thì in ra progress buôn ra thì in ra stop

---
+++ 32_SeekBar/SeekBar/app/src/main/java/com/example/seekbar/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("AAA", "Progress = " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("AAA", "Start");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("AAA", "Stop");
+            }
+        });
+    }
+}
