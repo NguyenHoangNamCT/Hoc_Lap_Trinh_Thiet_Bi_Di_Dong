0e9aecbeb47733b73b6b28b07766d2e33906747b
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-05-28 11:14:48 +0700

    Bắt sự kiện item click cho list view khi click vào item sẽ thông báo tên sự kiện, vị trí, giá trị và bắt sự kiện long item click tương tự như item click

--- 42_ListView/ListView_CoBan/app/src/main/java/com/example/listview_coban/MainActivity.java
+++ 42_ListView/ListView_CoBan/app/src/main/java/com/example/listview_coban/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listview_coban;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -30,5 +33,20 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, courseArrayList);
 
         lvMonHoc.setAdapter(adapter);
+
+        lvMonHoc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Click" + position + ": " + courseArrayList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        lvMonHoc.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Long click: " + position + ": " + courseArrayList.get(position), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
