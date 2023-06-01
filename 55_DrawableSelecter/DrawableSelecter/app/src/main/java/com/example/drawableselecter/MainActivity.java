9ccca2343e83fea521d5dadb52999cf15b11050f
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-06-01 20:29:46 +0700

    drawable selecter. Đổi màu nền và màu chữ button. Dùng drawable selecter tạo hiệu ứng khi click cụ thể là đổi màu chữ và màu button

---
+++ 55_DrawableSelecter/DrawableSelecter/app/src/main/java/com/example/drawableselecter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.drawableselecter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
