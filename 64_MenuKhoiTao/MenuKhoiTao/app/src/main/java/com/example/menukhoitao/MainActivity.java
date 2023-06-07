8b45a7eea08254854a0ee4ebd822555bbf1047a2
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-06-07 21:18:25 +0700

    Menu khởi tạo. Thêm toolbar cho ứng dụng, và thêm menu vào toolbar thêm menu vào item của menu, đặt icon cho menu

---
+++ 64_MenuKhoiTao/MenuKhoiTao/app/src/main/java/com/example/menukhoitao/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.menukhoitao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_demo, menu);
+        return true;
+    }
+}
