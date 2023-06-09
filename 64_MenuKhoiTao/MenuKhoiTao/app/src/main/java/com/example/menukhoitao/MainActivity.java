885762a42e2040ad4ee1fcded3e0b92a275589bc
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-06-09 20:09:22 +0700

    bắt sự kiện cho các item trong menu

--- 64_MenuKhoiTao/MenuKhoiTao/app/src/main/java/com/example/menukhoitao/MainActivity.java
+++ 64_MenuKhoiTao/MenuKhoiTao/app/src/main/java/com/example/menukhoitao/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.menukhoitao;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,5 +27,28 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_demo, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menuSettings){
+            Toast.makeText(this, "Bạn chọn setting", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.menuEmail){
+            Toast.makeText(this, "Bạn chọn email", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.menuPhone){
+            Toast.makeText(this, "Bạn chọn phone", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.menuSearch){
+            Toast.makeText(this, "Bạn chọn search", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.menuShare){
+            Toast.makeText(this, "Bạn chọn share", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.menuExit){
+            Toast.makeText(this, "Bạn chọn exit", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
